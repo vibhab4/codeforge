707b78a9f69caa915a98107f9db3243cbf88d16f
Vibha Bhavikatti <vibhabhavikatti@Vibhas-MacBook-Pro.local>
2025-04-28 12:58:32 -0700

    reset changes

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',  // <<< very important
+    setupFiles: './src/setupTests.ts',  
   },
   resolve: {
     alias: {
